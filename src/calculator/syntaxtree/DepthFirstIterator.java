9a350d0a22b3a1a1707fd71b0d2bc859bc1b61f8
Luca <luca777744@gmail.com>
2021-02-10 20:14:28 +0100

    DepthFirstIterator: added equals

--- src/calculator/syntaxtree/DepthFirstIterator.java
+++ src/calculator/syntaxtree/DepthFirstIterator.java
@@ -26,4 +26,39 @@ public class DepthFirstIterator {
 
         throw new RuntimeException("Instance root has bad type!");
     }
+
+    private static boolean equals(Visitable v1, Visitable v2)
+    {
+        if (v1 == v2)
+            return true;
+        if (v1 == null)
+            return false;
+        if (v2 == null)
+            return false;
+        if (v1.getClass() != v2.getClass())
+            return false;
+        if (v1.getClass() == IntegerNode.class)
+        {
+            IntegerNode op1 = (IntegerNode) v1;
+            IntegerNode op2 = (IntegerNode) v2;
+            return op1.value.equals(op2.value);
+        }
+        if (v1.getClass() == UnaryOpNode.class)
+        {
+            UnaryOpNode op1 = (UnaryOpNode) v1;
+            UnaryOpNode op2 = (UnaryOpNode) v2;
+            return op1.operator.equals(op2.operator) &&
+                    equals(op1.subNode, op2.subNode);
+        }
+        if (v1.getClass() == BinOpNode.class)
+        {
+            BinOpNode op1 = (BinOpNode) v1;
+            BinOpNode op2 = (BinOpNode) v2;
+            return op1.operator.equals(op2.operator) &&
+                    equals(op1.left, op2.left) &&
+                    equals(op1.right, op2.right);
+        }
+        throw new IllegalStateException("Ungueltiger Knotentyp");
+    }
+
 }
