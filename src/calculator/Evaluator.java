4f5bc1f5319f1b5ed31d3a0ca1d7b7e2aea7673b
TobseJo <tobi.emmer@t-online.de>
2021-02-10 14:19:20 +0100

    added evaluator and commented override methods

--- src/calculator/Evaluator.java
+++ src/calculator/Evaluator.java
@@ -1,4 +1,23 @@
 package calculator;
 
-public class Evaluator {
+import calculator.syntaxtree.Visitor;
+import calculator.syntaxtree.nodes.BinOpNode;
+import calculator.syntaxtree.nodes.IntegerNode;
+import calculator.syntaxtree.nodes.UnaryOpNode;
+
+public class Evaluator implements Visitor {
+    @Override
+    public void visit(IntegerNode node) {
+        //Falls der aktuelle Syntaxknoten ein Integer-Knoten (d.h. ein Blattknoten) ist tue nichts (, da bereits beim Anlegen des Integer-Knotens der aktuelle Zahlenwert gespeichert wird)
+    }
+
+    @Override
+    public void visit(BinOpNode node) {
+        //Falls der aktuelle Syntaxknoten ein unärer Operator-Knoten ist speichere -n im aktuellen Syntaxknoten, wobei n diejenige Zahl ist, die im direkten Kindknoten abgespeichert ist
+    }
+
+    @Override
+    public void visit(UnaryOpNode node) {
+        //Falls der aktuelle Syntaxknoten ein binärer Operator-Knoten ist◦speichere n op m im aktuellen Syntaxknoten, wobei n der Wert des linken direkten Kindknotens ist und m der Wert des rechten direkten Kindknotens
+    }
 }
