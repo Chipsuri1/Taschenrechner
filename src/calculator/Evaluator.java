7b97e900a1efb4a84020a98f21ddf431288cc487
TobseJo <tobi.emmer@t-online.de>
2021-02-14 17:11:32 +0100

    added RuntimeException if sub/right/left - node is not instance of SyntaxNode

--- src/calculator/Evaluator.java
+++ src/calculator/Evaluator.java
@@ -17,41 +17,47 @@ public class Evaluator implements Visitor {
     @Override
     public void visit(BinOpNode node) {
         //Falls der aktuelle Syntaxknoten ein binärer Operator-Knoten ist◦speichere n op m im aktuellen Syntaxknoten, wobei n der Wert des linken direkten Kindknotens ist und m der Wert des rechten direkten Kindknotens
-
-        SyntaxNode leftIntegerNode = (SyntaxNode) node.getLeft();
-        SyntaxNode rightIntegerNode = (SyntaxNode) node.getRight();
-        switch (node.getOperator()) {
-            case "+":
-                node.setValue(leftIntegerNode.getValue() + rightIntegerNode.getValue());
-                break;
-            case "-":
-                node.setValue(leftIntegerNode.getValue() - rightIntegerNode.getValue());
-                break;
-            case "*":
-                node.setValue(leftIntegerNode.getValue() * rightIntegerNode.getValue());
-                break;
-            case "/":
-                node.setValue(leftIntegerNode.getValue() / rightIntegerNode.getValue());
-                break;
-            case "^":
-                node.setValue((int) Math.pow(leftIntegerNode.getValue(), rightIntegerNode.getValue()));
-                break;
-            default:
-                throw new RuntimeException(("BinOpNode - forbidden Operator: " + node.operator));
+        if (node.getRight() instanceof SyntaxNode && node.getLeft() instanceof SyntaxNode) {
+            SyntaxNode leftIntegerNode = (SyntaxNode) node.getLeft();
+            SyntaxNode rightIntegerNode = (SyntaxNode) node.getRight();
+            switch (node.getOperator()) {
+                case "+":
+                    node.setValue(leftIntegerNode.getValue() + rightIntegerNode.getValue());
+                    break;
+                case "-":
+                    node.setValue(leftIntegerNode.getValue() - rightIntegerNode.getValue());
+                    break;
+                case "*":
+                    node.setValue(leftIntegerNode.getValue() * rightIntegerNode.getValue());
+                    break;
+                case "/":
+                    node.setValue(leftIntegerNode.getValue() / rightIntegerNode.getValue());
+                    break;
+                case "^":
+                    node.setValue((int) Math.pow(leftIntegerNode.getValue(), rightIntegerNode.getValue()));
+                    break;
+                default:
+                    throw new RuntimeException(("BinOpNode - forbidden Operator: " + node.operator));
+            }
+        } else {
+            throw new RuntimeException("Error - Node cannot be cast to UnaryOpNode!");
         }
     }
 
     @Override
     public void visit(UnaryOpNode node) {
         //Falls der aktuelle Syntaxknoten ein unärer Operator-Knoten ist speichere -n im aktuellen Syntaxknoten, wobei n diejenige Zahl ist, die im direkten Kindknoten abgespeichert ist
-        SyntaxNode subNode = (SyntaxNode) node.subNode;
-
-        switch (node.getOperator()) {
-            case "-":
-                node.setValue(-subNode.getValue());
-                break;
-            default:
-                throw new RuntimeException(("UnaryOpNode - forbidden Operator: " + node.operator));
+        if (node.subNode instanceof SyntaxNode) {
+            SyntaxNode subNode = (SyntaxNode) node.subNode;
+            switch (node.getOperator()) {
+                case "-":
+                    node.setValue(-subNode.getValue());
+                    break;
+                default:
+                    throw new RuntimeException(("UnaryOpNode - forbidden Operator: " + node.operator));
+            }
+        } else {
+            throw new RuntimeException("Error - Node cannot be cast to UnaryOpNode!");
         }
     }
 }
