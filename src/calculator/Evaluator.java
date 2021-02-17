8d5593ddf97d0fb93384efaf228d755dccd2b59f
TobseJo <tobi.emmer@t-online.de>
2021-02-17 16:37:01 +0100

    switch nici

--- src/calculator/Evaluator.java
+++ src/calculator/Evaluator.java
@@ -20,23 +20,12 @@ public class Evaluator implements Visitor {
             SyntaxNode leftIntegerNode = (SyntaxNode) node.getLeft();
             SyntaxNode rightIntegerNode = (SyntaxNode) node.getRight();
             switch (node.getOperator()) {
-                case "+":
-                    node.setValue(leftIntegerNode.getValue() + rightIntegerNode.getValue());
-                    break;
-                case "-":
-                    node.setValue(leftIntegerNode.getValue() - rightIntegerNode.getValue());
-                    break;
-                case "*":
-                    node.setValue(leftIntegerNode.getValue() * rightIntegerNode.getValue());
-                    break;
-                case "/":
-                    node.setValue(leftIntegerNode.getValue() / rightIntegerNode.getValue());
-                    break;
-                case "^":
-                    node.setValue((int) Math.pow(leftIntegerNode.getValue(), rightIntegerNode.getValue()));
-                    break;
-                default:
-                    throw new RuntimeException(("BinOpNode - forbidden Operator: " + node.operator));
+                case "+" -> node.setValue(leftIntegerNode.getValue() + rightIntegerNode.getValue());
+                case "-" -> node.setValue(leftIntegerNode.getValue() - rightIntegerNode.getValue());
+                case "*" -> node.setValue(leftIntegerNode.getValue() * rightIntegerNode.getValue());
+                case "/" -> node.setValue(leftIntegerNode.getValue() / rightIntegerNode.getValue());
+                case "^" -> node.setValue((int) Math.pow(leftIntegerNode.getValue(), rightIntegerNode.getValue()));
+                default -> throw new RuntimeException(("BinOpNode - forbidden Operator: " + node.operator));
             }
         } else {
             throw new RuntimeException("Error - Node cannot be cast to UnaryOpNode!");
@@ -49,11 +38,8 @@ public class Evaluator implements Visitor {
         if (node.subNode instanceof SyntaxNode) {
             SyntaxNode subNode = (SyntaxNode) node.subNode;
             switch (node.getOperator()) {
-                case "-":
-                    node.setValue(-subNode.getValue());
-                    break;
-                default:
-                    throw new RuntimeException(("UnaryOpNode - forbidden Operator: " + node.operator));
+                case "-" -> node.setValue(-subNode.getValue());
+                default -> throw new RuntimeException(("UnaryOpNode - forbidden Operator: " + node.operator));
             }
         } else {
             throw new RuntimeException("Error - Node cannot be cast to UnaryOpNode!");
